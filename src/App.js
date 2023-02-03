037a921cad9290c6bea7f52aa9d1b190b570e974
Dom Vournias <54661880+DomVournias@users.noreply.github.com>
2023-02-03 21:03:39 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useRef, useState } from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { marked } from "marked";
+import example from "../src/example.md";
 
 function App() {
+  // const [initialState, setInitialState] = useState("");
+  const [markdown, setMarkdown] = useState("");
+  const [html, setHtml] = useState("");
+
+  useEffect(() => {
+    fetch(example)
+      .then((response) => response.text())
+      .then((text) => {
+        setMarkdown(text);
+        setHtml(marked(text));
+      });
+  }, []);
+
+  console.log(markdown);
+
+  const handlePreview = (e) => {
+    setMarkdown(e.target.value);
+    setHtml(marked(e.target.value));
+  };
+
+  console.log(markdown);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <main>
+        <aside id="left">
+          <div id="inner">
+            <label htmlFor="editor" id="label">
+              Markdown
+            </label>
+            <textarea
+              id="editor"
+              name="editor"
+              value={markdown}
+              onChange={handlePreview}
+            />
+          </div>
+        </aside>
+        <aside id="right">
+          <div id="inner">
+            <label htmlFor="editor" id="label">
+              Preview
+            </label>
+
+            <div
+              id="preview"
+              name="preview"
+              dangerouslySetInnerHTML={{ __html: html }}
+            />
+          </div>
+        </aside>
+      </main>
+      <Footer />
+    </>
   );
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React, { useEffect, useRef } from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Markdown Live Previewer</h1>
+    </header>
+  );
+};
+
+export default Header;
